69f1c2612bc499b4188d36bad6934ec4c0e8af94
Joonhyup Lee <joonhyup@mit.edu>
2025-12-06 21:31:22 -0500

    Fix karatsuba bug

--- karatsuba.c
+++ karatsuba.c
@@ -3,18 +3,12 @@
 #include <time.h>
 #include <stdint.h>
 
-char *to_binary(uint64_t x) {
-  char* ret;
-  ret = malloc(65 * sizeof(char));
-  ret[64] = 0;
-  for (int i = 63; i >= 0; --i) {
-    ret[i] = '0' + (char)(x & 1);
-    x = x >> 1;
-  }
+typedef struct {
+  uint32_t msb;
+  uint64_t lower;
+} uint65_t;
 
-  return ret;
-}
-uint64_t karatsuba(uint32_t x, uint32_t y, uint64_t z) {
+uint65_t karatsuba(uint32_t x, uint32_t y, uint64_t z) {
   uint16_t x1 = x >> 16;
   uint16_t x2 = x & 0xffff;
   uint16_t y1 = y >> 16;
@@ -36,35 +30,57 @@ uint64_t karatsuba(uint32_t x, uint32_t y, uint64_t z) {
   uint64_t high = (uint64_t)x1 * (uint64_t)y1 + (uint64_t)z1; // Bit#(33)
   uint64_t low = (uint64_t)x2 * (uint64_t)y2 + (uint64_t)z2; // Bit#(33)
   uint32_t mid = (uint32_t)midx_val * (uint32_t)midy_val + ((uint32_t)adj << 16); // Bit#(32)
-  printf("midx: %d, midy: %d, midx_sign: %x, midy_sign: %x, mid: %u, mid_correct: %d\n",
-          (int32_t)midx, (int32_t)midy, midx_sign, midy_sign, mid, (int32_t)midx * (int32_t)midy);
+  int32_t mid_correct = (int32_t)midx * (int32_t)midy;
+  if (mid != (uint32_t)mid_correct)
+    printf("midx: %d, midy: %d, midx_sign: %x, midy_sign: %x, mid: %u, mid_correct: %d\n",
+            (int32_t)midx, (int32_t)midy, midx_sign, midy_sign, mid, mid_correct);
 
-  uint64_t mid_ = (midx_sign ^ midy_sign ? (uint64_t)(-1) << 32 : 0) | mid; // signExtend to Bit#(33)
+  uint64_t mid_ = (midx_sign ^ midy_sign && midx && midy ? (uint64_t)(-1) << 32 : 0) | mid; // signExtend to Bit#(33)
   mid_ += low + high - midz;
-  printf("mid_: %llu, mid_correct: %llu\n",
-          mid_, (uint64_t)x1 * (uint64_t)y2 + (uint64_t) x2 * (uint64_t) y1);
+  uint64_t mid__correct = (uint64_t)x1 * (uint64_t)y2 + (uint64_t) x2 * (uint64_t) y1;
+  if (mid_ != mid__correct)
+    printf("mid_: %llu, mid__correct: %llu\n", mid_, mid__correct);
   mid_ += low >> 16;
   high += (uint32_t)(mid_ >> 16);
-  return ((uint64_t)high << 32) | ((mid_ & 0xffff) << 16) | (low & 0xffff);
+
+  uint65_t ret;
+  ret.msb = (uint32_t)(high >> 32);
+  ret.lower = ((uint64_t)high << 32) | ((mid_ & 0xffff) << 16) | (low & 0xffff);
+  return ret;
 }
 
 int main () {
   uint32_t x, y;
-  uint64_t z;
+  uint64_t z, mult, diff_lower;
+  uint65_t karatsuba_out, correct;
+  char diff_msb;
+
   srand(time(NULL));
 
-  x = rand() & 0xffff;
-  x = x << 16;
-  x |= rand() & 0xffff;
-  y = rand() & 0xffff;
-  y = y << 16;
-  x |= rand() & 0xffff;
-  z = rand();
-  z = z << 32;
-  z |= rand();
+  for (int i = 0; i < 100000000; ++ i) {
+    x = rand() & 0xffff;
+    x = x << 16;
+    x |= rand() & 0xffff;
+    y = rand() & 0xffff;
+    y = y << 16;
+    x |= rand() & 0xffff;
+    z = rand();
+    z = z << 32;
+    z |= rand();
 
-  uint64_t karatsuba_out = karatsuba(x, y, -z);
-  uint64_t correct = (uint64_t)x * (uint64_t)y - z;
-  printf("x: %u, y: %u, z: %llu, diff: %s\n", x, y, z, to_binary(karatsuba_out ^ correct));
+    karatsuba_out = karatsuba(x, y, z);
+    mult = (uint64_t)x * (uint64_t)y;
+    correct.msb = z && (mult >= -z);
+    correct.lower = mult + z;
+    diff_msb = karatsuba_out.msb ^ correct.msb;
+    diff_lower = karatsuba_out.lower ^ correct.lower;
+    if (diff_msb || diff_lower) {
+      if (correct.msb > 0) {
+        printf("msb is 1\n");
+      }
+      printf("x: %u, y: %u, z: %llu, diff_msb: %c, diff_lower: %llx\n",
+              x, y, z, diff_msb ? '1' : '0', diff_lower);
+    }
+  }
   return 0;
 }
